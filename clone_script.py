d559d59629f3d29992f674c2a33b35668fc518df
Ghred dase <129958145+deswafyfxd@users.noreply.github.com>
2024-12-11 11:55:29 +0530

    Update clone_script.py

--- clone_script.py
+++ clone_script.py
@@ -1,27 +1,31 @@
 import os
 import subprocess
 
-def clone_repo(repo_url, custom_name):
+def clone_repo(full_url, custom_name):
     """
     Clone a repository from the given URL into a custom directory.
     
     Args:
-    repo_url (str): The URL of the repository to be cloned.
+    full_url (str): The full URL with credentials of the repository to be cloned.
     custom_name (str): The name of the directory to clone the repository into.
     """
     
     try:
         # Run the git clone command with custom directory name
-        result = subprocess.run(['git', 'clone', repo_url, custom_name], check=True)
-        print(f"Repository cloned successfully into {custom_name}: {repo_url}")
+        result = subprocess.run(['git', 'clone', full_url, custom_name], check=True)
+        print(f"Repository cloned successfully into {custom_name}: {full_url}")
     except subprocess.CalledProcessError as e:
         print(f"Error occurred while cloning the repository: {e}")
-        
+
 # Example usage
 if __name__ == "__main__":
     repo_url = os.environ.get('REPO_URL')
+    username = os.environ.get('BITBUCKET_USERNAME')
+    password = os.environ.get('BITBUCKET_APP_PASSWORD')
     custom_name = os.environ.get('CUSTOM_NAME', 'cloned_repo')
-    if repo_url:
-        clone_repo(repo_url, custom_name)
+
+    if repo_url and username and password:
+        full_url = f"https://{username}:{password}@{repo_url}"
+        clone_repo(full_url, custom_name)
     else:
-        print("Repository URL not provided.")
+        print("Required environment variables are not provided.")
